78827d3d1b41536fe70357cade71b44326ca4318
Anastasiia Yanovna <a.harinskaya@yandex.ru>
2024-01-31 23:56:10 +0300

    Выполнено задание 1 из темы Абстрактные классы, виртуальные методы, интерфейс

---
+++ Abstracts_Task1/Figure.cs
@@ -0,0 +1,9 @@
+﻿namespace Abstract_Task1
+{
+    internal abstract class Figure
+    {
+        protected Figure() { }
+        public abstract float GetPerimeter();
+        public abstract float GetArea();
+    }
+}
