3a29dc34e99b66863d3d8e2d44b9df602e03027b
Anastasiia Yanovna <a.harinskaya@yandex.ru>
2024-02-25 18:47:17 +0300

    Выполнено задани 3 из темы Коллекции

---
+++ Collections_Task3/TaskExecution.cs
@@ -0,0 +1,20 @@
+﻿/*Задание 3:
+Коллекция содержит набор чисел в виде одной строки "1, 2, 3, 4, 4, 5". 
+Избавиться от повторяющихся элементов в строке и вывести результат на экран.
+*/
+
+namespace Collections_Task3;
+public class TaskExecution
+{
+    static void Main()
+    {
+        List<string> numbersList = new List<string>() { "1 2, 3, 4, 4, 5" };
+        string[] numbersArray = numbersList[0].Split(", "); //не могу убрать повтор пока не разделю строку
+        var hashset = new HashSet<string>(numbersArray);
+
+        foreach (var num in hashset)
+        {
+            Console.Write($"{num}, ");
+        }
+    }
+}
