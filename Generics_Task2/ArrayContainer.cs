5dd822f36f4885803802de3bce6bb081aa653569
Anastasiia Yanovna <a.harinskaya@yandex.ru>
2024-02-25 23:56:13 +0300

    Выполнено задание 2 из темы Обобщения

---
+++ Generics_Task2/ArrayContainer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Task2
+{
+    internal class ArrayContainer<T>
+    {
+        private T[] arrayContainer;
+
+        public ArrayContainer()
+        {
+            arrayContainer = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] newArray = new T[arrayContainer.Length + 1];
+            arrayContainer.CopyTo(newArray, 0);
+            newArray[arrayContainer.Length] = item;
+            arrayContainer = newArray;
+        }
+
+        public void Remove(int index)
+        {
+            T[] newArray = new T[arrayContainer.Length - 1];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = arrayContainer[i];
+            }
+            // сам index пропускается
+            for (int i = index + 1; i < arrayContainer.Length; i++)
+            {
+                newArray[i - 1] = arrayContainer[i];
+            }
+            arrayContainer = newArray;
+        }
+
+        public void Find(int index)
+        {
+            Console.WriteLine(arrayContainer[index]);
+        }
+
+        public void Length()
+        {
+            Console.WriteLine(arrayContainer.Length);
+        }
+    }
+}
+
+
---
+++ Generics_Task2/TaskExecution.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задание 2:
+Напишите обобщенный класс, который может хранить в массиве объекты любого типа. 
+Также, данный класс должен иметь методы для добавления данных в массив, удаления из массива, получения элемента из массива по индексу и метод, возвращающий длину массива.
+Для упрощения работы можно пересоздавать массив при каждой операции добавления и удаления
+*/
+
+namespace Generics_Task2;
+internal class TaskExecution
+{
+    static void Main()
+    {
+        ArrayContainer<int> сontainerInt = new ArrayContainer<int>();
+
+        сontainerInt.Add(1);
+        сontainerInt.Add(2);
+        сontainerInt.Remove(0);
+        сontainerInt.Find(0);
+        сontainerInt.Length();
+
+        ArrayContainer<string> сontainerStr = new ArrayContainer<string>();
+        сontainerStr.Add("5");
+        сontainerStr.Add("TEST");
+        сontainerStr.Remove(0);
+        сontainerStr.Find(0);
+        сontainerStr.Length();
+    }
+}
