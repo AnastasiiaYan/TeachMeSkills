6d285445b5f438eed94c04bbae2de7bf15db3b90
Anastasiia Yanovna <a.harinskaya@yandex.ru>
2024-03-03 00:31:58 +0300

    Выполнено задание 2 из темы Делегаты

---
+++ Delegates_Task2/CalculationMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Task2
+{
+    internal class CalculationMethods
+    {
+        public double Lenth(double r)
+        {
+            double result = 2 * Math.PI * r;
+            Console.WriteLine($"Длина окружности: {result}");
+            return result;
+        }
+
+        public double Area(double r)
+        {
+            double result = Math.PI * r * r;
+            Console.WriteLine($"Площадь круга: {result}");
+            return result;
+        }
+
+        public double Volume(double r)
+        {
+            double result = (4 / 3) * Math.PI * r * r * r;
+            Console.WriteLine($"Объем шара: {result}");
+            return result;
+        }
+    }
+}
