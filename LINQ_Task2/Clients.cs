1ea9358102103e745c66c3dc907d7041c178b989
Anastasiia Yanovna <a.harinskaya@yandex.ru>
2024-03-13 22:04:37 +0300

    Выполнены задания по теме LINQ

---
+++ LINQ_Task2/Clients.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Task2
+{
+    internal class Clients
+    {
+        internal int code;
+        internal int year;
+        internal int month;
+        internal int trainingDuration;
+
+        public Clients(int code, int year, int month, int trainingDuration)
+        {
+            this.code = code;
+            this.year = year;
+            this.month = month;
+            this.trainingDuration = trainingDuration;
+        }        
+    }
+}
